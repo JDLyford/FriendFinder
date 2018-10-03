84395071e829380130a35d17f30f65973ded8470
Joseph Lyford <lyford.joseph@yahoo.com>
2018-10-02 21:50:15 -0400

    added in friend pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@
 var friends = [
   {
     name: "Stephen",
-    photo: "",
+    photo: "https://image.shutterstock.com/image-vector/hot-dog-cartoon-character-celebrating-260nw-147154727.jpg",
     scores: [
       "2",
       "4",
@@ -20,7 +20,7 @@ var friends = [
   },
   {
     name: "William",
-    photo: "",
+    photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/chubby-guard-the-powerpuff-girls-46.3.jpg",
     scores: [
       "1",
       "5",
@@ -36,7 +36,7 @@ var friends = [
   },
   {
     name: "Palmer",
-    photo: "",
+    photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjIvM3Ek-ndAhVRrlkKHTLPBdYQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Ftheirishgamer5&psig=AOvVaw0OeYCdD-d8_IHy3uynPpPq&ust=1538617634367761",
     scores: [
       "2",
       "5",
@@ -52,7 +52,7 @@ var friends = [
   },
   {
     name: "Christian",
-    photo: "",
+    photo: "https://img.bleacherreport.net/img/images/photos/001/869/601/hi-res-148455070_crop_exact_original_original_crop_exact.jpg?w=650&h=433&q=75",
     scores: [
       "3",
       "3",
@@ -68,7 +68,7 @@ var friends = [
   },
   {
     name: "Brian",
-    photo: "",
+    photo: "https://orig00.deviantart.net/2158/f/2008/096/a/c/army_guy_by_chara89.jpg",
     scores: [
       "1",
       "3",
@@ -84,7 +84,7 @@ var friends = [
   },
   {
     name: "Kaley",
-    photo: "",
+    photo: "https://media.istockphoto.com/illustrations/the-boy-regretted-the-ice-cream-falling-illustration-id518928459?k=6&m=518928459&s=612x612&w=0&h=pgV3zfXfnjue1r2VXQPJ9Yw4_7sEN5LMZDcM3t42Q5o=",
     scores: [
       "1",
       "1",
@@ -100,7 +100,7 @@ var friends = [
   },
   {
     name: "Pam",
-    photo: "",
+    photo: "https://img.purch.com/rc/300x200/aHR0cHM6Ly93d3cuYnVzaW5lc3NuZXdzZGFpbHkuY29tL2ltYWdlcy9pLzAwMC8wMTYvNjEwL29yaWdpbmFsL0FjY291bnRpbmdfcGF0dGFyYXdhdC5qcGc=",
     scores: [
       "2",
       "5",
